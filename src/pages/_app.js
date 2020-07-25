3ec96ea7ec9cc772b170b181df4f98f1352de64f
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-07-25 15:31:28 +0530

    Updated _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/index.css'
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
+import "../styles/index.css";
+import "../styles/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
