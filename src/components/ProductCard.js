ed7f77b2715483c84d3510496edde016a123f476
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-07-26 12:35:29 +0530

    Product Card UI done

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import Ratings from "react-star-rating-component";
 
 const ProductCard = ({ product }) => {
+  const [quantity, setQuantity] = useState(0);
+
   return (
     <div className="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden my-10">
       <div className="px-4 py-2">
@@ -19,14 +22,55 @@ const ProductCard = ({ product }) => {
       />
       <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
         <h1 className="text-gray-200 font-bold text-xl">₹{product.price}</h1>
-        <button className="px-3 py-1 bg-gray-200 text-sm text-gray-900 font-semibold rounded">
-          Add to cart
-        </button>
+        {quantity === 0 ? (
+          <button
+            onClick={() => setQuantity((prev) => prev + 1)}
+            className="px-3 py-1 bg-gray-200 text-sm text-gray-900 font-semibold rounded"
+          >
+            Add to cart
+          </button>
+        ) : (
+          <div className="flex justify-evenly">
+            <button
+              onClick={() => {
+                if (quantity >= 0) setQuantity((prev) => prev - 1);
+              }}
+              className="bg-gray-200 px-3 py-1 text-sm text-gray-900 font-extrabold focus:outline-none "
+              disabled={quantity < 0}
+            >
+              -
+            </button>
+            <div className="px-3 py-1 bg-gray-200 text-sm text-green-600 font-extrabold ">
+              {quantity}
+            </div>
+            <button
+              onClick={() => setQuantity((prev) => prev + 1)}
+              className="bg-gray-200 px-3 py-1  text-sm text-green-600 font-bold focus:outline-none "
+            >
+              +
+            </button>
+          </div>
+        )}
       </div>
-      <div class="my-6 pt-3 flex flex-wrap mx-6 border-t">
+      <div className="flex justify-between px-4 py-2 bg-gray-900">
+        <Ratings
+          name="rate1"
+          starCount={5}
+          value={product.rating}
+          starColor="#f00"
+          emptyStarColor="grey"
+          editing={false}
+          className="text-2xl"
+        />
+        <h1 className="text-white">Ratings: {product.reviews}</h1>
+      </div>
+      <div className="my-6 pt-3 flex flex-wrap mx-6 border-t">
         {product.tags.map((tag) => {
           return (
-            <div class="text-xs mr-2 my-1 uppercase tracking-wider border px-2 text-indigo-600 border-indigo-600 hover:bg-indigo-600 hover:text-indigo-100 cursor-default">
+            <div
+              key={product.tags.indexOf(tag)}
+              className="text-xs mr-2 my-1 uppercase tracking-wider border px-2 text-indigo-600 border-indigo-600 hover:bg-indigo-600 hover:text-indigo-100 cursor-default"
+            >
               {tag}
             </div>
           );
