277b53cd5c24f401bd6b949d809dd73447f9ea07
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-20 06:50:07 +0530

    initialized Stripe

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,21 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 import "../styles/index.css";
 import "../styles/tailwind.css";
 
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <Elements stripe={stripePromise}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </Elements>
   );
 }
 
