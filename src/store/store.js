8fd00b28c0fd5fba139e50c88f5e7c2c4c7cabc0
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-20 06:49:07 +0530

    added wishlist to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { updateCart as cart } from "./cart/cartReducer";
-// import { productsReducer as products } from "./products/reducers";
-import cartReducer from "./cartSlice";
-import productsReducer from "./productCardSlice";
+import {
+  loadFromLocalStorage,
+  saveToLocalStorage,
+} from "../services/localStorage";
+import cart from "./cartSlice";
+import products from "./productCardSlice";
+import wishlists from "./wishlistSlice";
 
+const persistedState = loadFromLocalStorage();
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
-    products: productsReducer,
+    cart,
+    products,
+    wishlists,
   },
 });
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
