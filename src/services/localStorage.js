61ee08adf180e006b3e3ff22a76fadefa8e5c7a9
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-20 06:48:42 +0530

    added localStorage for redux persist

---
+++ src/services/localStorage.js
@@ -0,0 +1,19 @@
+export const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+};
