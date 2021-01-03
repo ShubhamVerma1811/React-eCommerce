4bb77351923ba6bac7647ae77c0d20ecc487099a
Shubham Verma <imshubhamverma.sv@gmail.com>
2021-01-03 13:02:03 +0530

    removed log in link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -43,11 +43,6 @@ const NavBar = () => {
                 Wishlists
               </a>
             </Link>
-            <Link href="/login">
-              <a className=" block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0">
-                Log In
-              </a>
-            </Link>
           </div>
         </div>
       </nav>
